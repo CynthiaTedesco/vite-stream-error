f5cf6bc4a37b76c35ad20e9af9ee9916a3d6da13
Cynthia Tedesco <cynthia.tedesco@gmail.com>
2021-06-15 11:52:02 +0200

    Remove unused code

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,12 @@
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-// import { viteCommonjs } from '@originjs/vite-plugin-commonjs'
-import 'getstream';
+import stream from 'getstream';
 
 // instantiate a new client (server side)
 // const client = stream.connect(
-//   '92mzjgtz5tvc',
-//   'pbrxdneuga6wxswydvkvhrhmmher4yhpbmtjeudj3f5pm4chquwmveu4jmb23xqd'
+//   '',
+//   ''
 // );
 
 export default {
@@ -19,13 +18,6 @@ export default {
   },
 }
 
-    // const client = stream.connect(
-    //   '92mzjgtz5tvc',
-    //   'pbrxdneuga6wxswydvkvhrhmmher4yhpbmtjeudj3f5pm4chquwmveu4jmb23xqd'
-    // );
-
-// This starter template is using Vue 3 experimental <script setup> SFCs
-// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
 </script>
 
 <style>
